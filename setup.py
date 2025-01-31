b961a8c905e6da3853374f9c269bb62520d0001c
Zeyu Deng (Jerry) <dengzeyu@gmail.com>
2025-02-01 01:26:15 +0800

    Update setup.py to include maintainer info, license, and long description

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
+
+__version__ = "0.1"
+
 setup(
     name="mle3112",
-    version="0.1",
+    version=__version__,
     author="Zeyu Deng",
     author_email="dengzeyu@gmail.com",
+    maintainer="Zeyu Deng",
+    maintainer_email="dengzeyu@gmail.com",
     install_requires=[
         "git+https://github.com/dengzeyu/NUS_experiment.git",
-    ]
+    ],
+    license="MIT License",
+    long_description=open("README.md").read(),
+    python_requires=">=3.8",
+    package_dir={"mle3112": "mle3112"},
 )
